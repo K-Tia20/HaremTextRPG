96c1947d7996e361e151e7e053f4a4bcee9cceff
문승종 <jongcc2345@gmail.com>
2026-05-22 11:55:58 +0900

    버그수정
    Player에 복사 된 P_Harem.cpp 삭제

--- P_Harem/Objects/Items/Item.h
+++ P_Harem/Objects/Items/Item.h
@@ -1,8 +1,23 @@
 #pragma once
+#include <iostream>
+#include <string>
+
+struct ItemDatas
+{
+	 std::string name;
+	int price;
+	
+};
 
 class Items
 {
+public:
+	Items();
+	~Items();
 
+
+private:
+	int price;
 
 };
 
