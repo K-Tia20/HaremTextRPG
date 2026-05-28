fff7d0dde0783da85f596eb1e06d17676176427f
문승종 <jongcc2345@gmail.com>
2026-05-29 03:20:45 +0900

    보스에게 패배후 엔딩 구현
    보스인트로, 보스에게 패배엔딩 구현

--- P_Harem/Battle/BattleSystem.cpp
+++ P_Harem/Battle/BattleSystem.cpp
@@ -5,7 +5,13 @@
 #include "../Player/Player.h"
 #include "../Objects/Items/Item.h"
 #include "../Creature/Boss.h"
+#include "../UI/ImageManager.h"
+#include "../UI/LogSystem.h"
+#include "../UI/ScriptManager.h"
+
 #include <cmath>
+
+#include "../UI/UIManager.h"
 
 C_LevelSystem LevelSystem; 
 
@@ -137,14 +143,22 @@ void C_BattleSystem::BossBattle(std::shared_ptr<C_Creature> Player)
 		{
 			if (OnDefeat) OnDefeat(Player->GetName());
 
-			W_Player->GetGirlFrends().erase(std::remove(
-				W_Player->GetGirlFrends().begin(),
-				W_Player->GetGirlFrends().end(),
-				Player),
-				W_Player->GetGirlFrends().end());
-			
-			if (!W_Player->GetGirlFrends().empty()) Player = W_Player->SetFightGirl();
-			else { World->BadEnd(); return; } // 여친 다 죽으면 배드엔딩
+			W_Player->RemoveGirlFriend(Player);
+
+			if (W_Player->GetGirlFrends().empty())
+			{
+				World->BadEnd();
+				
+				return;
+			}
+
+			Player = W_Player->SetFightGirl();
+
+			if (!Player)
+			{
+				World->BadEnd();
+				return;
+			}
 		}
 
 		if (rand() % 100 < 60) // 40% 확률로 아이템 사용 시도
@@ -165,7 +179,8 @@ void C_BattleSystem::BossBattle(std::shared_ptr<C_Creature> Player)
 		if (Boss->IsDefeated())
 		{
 			if (OnDefeat) OnDefeat(Boss->GetName());
-			LevelSystem.GainAffinity(Player, 50); 
+			LevelSystem.GainAffinity(Player, 50);
+			World->RealEnd();
 			break;
 		}
 	}
@@ -173,5 +188,50 @@ void C_BattleSystem::BossBattle(std::shared_ptr<C_Creature> Player)
 
 void C_BattleSystem::BossBattleIntro(std::shared_ptr<C_Creature> Player, std::shared_ptr<C_Creature> Boss)
 {
-	
+	auto ui = World->GetUI();
+
+	if (!ui) return;
+
+	auto& script = C_ScriptManager::GetInstance();
+	auto& img = C_ImageManager::GetInstance();
+
+	ui->ClearMainViewport();
+	ui->CenteredTypeLog(script.Get("BATTLE_BOSS_INTRO0"), 18, 100);
+	ui->DrawImage(C_ImageManager::GetInstance().GetLayeredImage("BG_HunPo", {}));
+	Sleep(500);
+	ui->CenteredTypeLog(script.Get("BATTLE_BOSS_INTRO1"), 18, 100);
+	ui->DrawImage(C_ImageManager::GetInstance().GetLayeredImage("BG_HunPo", {}));
+	Sleep(500);
+	ui->ClearMainViewport();
+	ui->CenteredTypeLog(script.Get("BATTLE_BOSS_INTRO2"), 18, 100);
+	Sleep(500);
+	ui->ClearMainViewport();
+	ui->CenteredTypeLog(script.Get("BATTLE_BOSS_INTRO3"), 18, 100);
+	Sleep(500);
+	ui->DrawImage(C_ImageManager::GetInstance().GetLayeredImage("BG_HunPo", {}));
+	Sleep(500);
+	ui->CenteredTypeLog(script.Get("BATTLE_BOSS_INTRO4"), 18, 100);
+	Sleep(500);
+	ui->DrawImage(C_ImageManager::GetInstance().GetLayeredImage("BG_Boss", {}));
+	Sleep(500);
+	ui->CenteredTypeLog(script.Get("BATTLE_BOSS_INTRO5"), 18, 100);
+	Sleep(500);
+	ui->DrawImage(C_ImageManager::GetInstance().GetLayeredImage("BG_Boss", {}));
+	Sleep(500);
+	ui->CenteredTypeLog(script.Get("BATTLE_BOSS_INTRO6"), 18, 100);
+	Sleep(500);
+	ui->DrawImage(C_ImageManager::GetInstance().GetLayeredImage("BG_Boss", {}));
+	Sleep(500);
+	ui->CenteredTypeLog(script.Get("BATTLE_BOSS_INTRO7"), 18, 100);
+	Sleep(500);
+	ui->DrawImage(C_ImageManager::GetInstance().GetLayeredImage("BG_Boss", {}));
+	Sleep(500);
+
+	UIManager::WaitKey(ui);
+
+	std::string layeredImg = C_ImageManager::GetInstance().GetLayeredImage("BG_HunPo", {
+	{"CH_Hanma", 50, 0, false} // {캐릭터키, X위치(%), Y위치, 좌우반전여부}
+		});
+	ui->DrawImage(layeredImg);
+
 }
